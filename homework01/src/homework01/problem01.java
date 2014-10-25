840191afbf67c496089bad109264c00f720e33aa
sissie <sissie@abv.bg>
2014-10-25 18:25:22 +0300

    Restore lost code

--- homework01/src/homework01/problem01.java
+++ homework01/src/homework01/problem01.java
@@ -1,10 +1,15 @@
 package homework01;
-
+import java.util.*;
 public class problem01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Random rand = new Random();
+		int dice1, dice2;
+		dice1 = rand.nextInt(6) + 1;
+		dice2 = rand.nextInt(6) + 1;
+		if (dice1==dice2)System.out.println("Congrats! You hawe even!");
+		System.out.println("You have:"+ dice1+" and "+dice2);
+		System.out.println("Sum:"+ (dice1+dice2));
 	}
 
 }
